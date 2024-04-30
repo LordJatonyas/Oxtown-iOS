ad36a428b40bbd895aa58792792176f1af53cf35
Lee Chih Jung <john.12012000@gmail.com>
2024-04-30 13:50:08 +0100

    Initial Commit

---
+++ oxery/oxeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  oxeryApp.swift
+//  oxery
+//
+//  Created by John Lee on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct oxeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
